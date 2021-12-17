0295ee022e9ee09915b03c21bb3b61be0fa28ea2
Satish <satish.dexbytes@gmail.com>
2021-12-17 17:36:11 +0530

    work on video call

---
+++ src/joinroom.js
@@ -0,0 +1,111 @@
+import React, { useState, useCallback, useEffect } from "react";
+import Video from "twilio-video";
+import JoinRoomForm from "./components/joinroomform";
+import Room from "./Room";
+
+const JoinRoom = () => {
+  const [username, setUsername] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [room, setRoom] = useState(null);
+  const [error, setError] = useState(null);
+  const [connecting, setConnecting] = useState(false);
+
+  const handleUsernameChange = useCallback((event) => {
+    setUsername(event.target.value);
+  }, []);
+
+  const handleRoomNameChange = useCallback((event) => {
+    setRoomName(event.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+      setConnecting(true);
+      const response = await fetch("/v1/video/join-room", {
+        method: "POST",
+        body: JSON.stringify({
+          identity: username,
+          roomname: roomName,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+      if(response.success && response.success.data){
+        Video.connect(response.success.data.token, {
+          name: roomName,
+        })
+          .then((room) => {
+            setConnecting(false);
+            setRoom(room);
+          })
+          .catch((err) => {
+            console.error(err);
+            setConnecting(false);
+          });
+      }else{
+        setError(response.message)
+      }
+     
+    },
+    [roomName, username]
+  );
+
+  const handleLogout = useCallback(() => {
+    setRoom((prevRoom) => {
+      if (prevRoom) {
+        prevRoom.localParticipant.tracks.forEach((trackPub) => {
+          trackPub.track.stop();
+        });
+        prevRoom.disconnect();
+      }
+      return null;
+    });
+  }, []);
+
+  useEffect(() => {
+    if (room) {
+      const tidyUp = (event) => {
+        if (event.persisted) {
+          return;
+        }
+        if (room) {
+          handleLogout();
+        }
+      };
+      window.addEventListener("pagehide", tidyUp);
+      window.addEventListener("beforeunload", tidyUp);
+      return () => {
+        window.removeEventListener("pagehide", tidyUp);
+        window.removeEventListener("beforeunload", tidyUp);
+      };
+    }
+  }, [room, handleLogout]);
+
+  let render;
+  if (room) {
+    render = (
+      <Room roomName={roomName} room={room} handleLogout={handleLogout} />
+    );
+  } else {
+
+    render = (
+
+      <>
+        <JoinRoomForm
+          username={username}
+          roomName={roomName}
+          handleUsernameChange={handleUsernameChange}
+          handleRoomNameChange={handleRoomNameChange}
+          handleSubmit={handleSubmit}
+          connecting={connecting}
+          error={error}
+        />
+      </>
+    );
+  }
+  return render;
+};
+
+export default JoinRoom;
