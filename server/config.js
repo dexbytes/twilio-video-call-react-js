20fd1fabce357dbdfcdfe6d701547015cff886ae
Satish <satish.dexbytes@gmail.com>
2021-12-20 12:13:48 +0530

    work on audio voice

--- server/config.js
+++ server/config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  twilio: {
-    accountSid: "ACd00d9d74c41b31b01ff8e67d54deaab7",
-    apiKey: "SK2fcd2afb6d70cda43b917f736ce01cd4",
-    apiSecret: "oV4cVcdyspQOjzjFwV33UbDmsIgMfcMY"
-  },
   base_url:"http://localhost:3002"
 };
